2959a50d9a93a90d6b6d66370686e6c19ef9e9c8
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-09-09 22:20:56 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LightningList::Application.config.secret_key_base = '5c29b310bb67f5bf2e8ca26c663a47b92a8030372d78aaf04f31c42cb8ed0ee5fa1454d8bceb7f0fd650aa7f2d9523b9411d351cf62d9b614ac290c53c9c740c'
